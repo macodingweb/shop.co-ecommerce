b0237191b2b88e5018333aae3035fd499d9fbbbb
MACoding4U <mohammed711adel@gmail.com>
2025-02-26 15:43:09 +0200

    Initial Commit

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,10 +1,10 @@
-import Navbar from "@/components/Navbar";
 import { ReactNode } from "react";
+// import Navbar from "@/components/Navbar";
 
 export default function HomeLayout({ children } : { children: ReactNode }) {
   return (
     <main>
-      <Navbar />
+      {/* <Navbar logo={""} links={[]} /> */}
       { children }
     </main>
   )

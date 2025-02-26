6f95432ea77e526b0ce755846254527ae3846d2a
MACoding4U <mohammed711adel@gmail.com>
2025-02-26 15:40:16 +0200

    Initial  Commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,24 @@
-export default function Navbar() {
+import Image from "next/image"
+import Link from "next/link"
+
+interface NavTypes {
+  logo: string,
+  links: { url: string, text: string }[],
+}
+
+export default function Navbar({ logo, links } : NavTypes) {
   return (
     <nav>
-      Navbar
+      <div className="logo">
+        <Image src={logo} alt="Logo" width={120} height={120} />
+      </div>
+      <ul className="main-links">
+        { links.length > 0 && links.map((ele, index) => (
+          <li className="list-item" key={index}>
+            <Link href={ele.url}></Link>
+          </li>
+        )) }
+      </ul>
     </nav>
   )
 }
